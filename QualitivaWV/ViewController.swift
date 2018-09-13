0002adfe9fd6ad19976e4a689fb2dfee80d34c29
Laminal Falah <laminalfalah@icloud.com>
2018-09-13 09:43:32 +0700

    Initial Commit

---
+++ QualitivaWV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QualitivaWV
+//
+//  Created by Laminal Falah on 13/09/18.
+//  Copyright © 2018 Laminal Falah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
